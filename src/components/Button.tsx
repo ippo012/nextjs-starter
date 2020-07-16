6ab2e7cce7833bfc06c9d4264d1aa82de96fdc22
ippo012 <ippo@ginco.io>
2020-07-16 12:15:40 +0900

    :wrench: Fix components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,11 @@ export const Button = (props: ButtonProps) => {
   const { buttonProps } = useButton(props, ref);
 
   return (
-    <button {...buttonProps} ref={ref}>
+    <button
+      {...buttonProps}
+      ref={ref}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
       {props.children}
     </button>
   );
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -13,9 +13,15 @@ export const Checkbox = (props: CheckboxProps) => {
   const { inputProps } = useCheckbox(props, state, ref);
 
   return (
-    <label style={{ display: 'block' }}>
-      <input {...inputProps} ref={ref} />
-      {props.children}
-    </label>
+    <div className="flex items-center">
+      <input
+        {...inputProps}
+        ref={ref}
+        className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+      />
+      <label style={{ display: 'block' }} className="ml-2 block text-sm leading-5 text-gray-900">
+        {props.children}
+      </label>
+    </div>
   );
 };
